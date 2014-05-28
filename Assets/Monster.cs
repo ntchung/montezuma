d2ea6730095cc7d4a197583735d66142ce6512cb
Nguyen Chi Thien <nc.thien@gloops.com>
2014-05-29 00:04:03 +0700

    Test monster's animation

---
+++ Assets/Monster.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Monster : MonoBehaviour {
+
+	string curAnimName;
+
+	// Use this for initialization
+	void Start () {
+		curAnimName = animation.animation.name;
+	}
+
+	void SetAnimation(string name)
+	{
+		if (name != curAnimName)
+		{
+			curAnimName = name;
+			animation.CrossFade(name);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float move = Input.GetAxis("Horizontal");
+		if (move != 0.0f) SetAnimation("walk");
+		else SetAnimation("idle");
+	}
+}
