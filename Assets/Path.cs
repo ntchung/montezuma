a3c12c8509997f89d68a0002f37e7a976c082bfb
Nguyen Chi Thien <nc.thien@gloops.com>
2014-05-29 23:31:49 +0700

    - Add structure for polyline path

---
+++ Assets/Path.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Path : MonoBehaviour 
+{
+	public GameObject[] beacons;
+
+	private List<Vector3> points = null;
+
+	// Use this for initialization
+	void Start () 
+	{
+		points = new List<Vector3>();
+		for (int i = 0; i < beacons.Length; i++)
+		{
+			Vector3 pos = beacons[i].transform.position;
+			pos.y = 10.0f;
+			points.Add(pos);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	public void OnDrawGizmos()
+	{
+		if (points == null) return;
+
+		for (int i = 0; i < points.Count - 1; i++)
+		{
+			Gizmos.DrawLine(points[i], points[i + 1]);
+		}
+	}
+}
